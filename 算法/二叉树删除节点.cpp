f34cbe6c71098aa24aa8dbb92fa5f4e001b0612a
as010101 <37323057+as010101@users.noreply.github.com>
2019-08-23 11:17:11 +0800

    Create 二叉树删除节点.cpp

---
+++ 算法/二叉树删除节点.cpp
@@ -0,0 +1,73 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  
+  
+  
+    TreeNode* deleteNode(TreeNode* root, int key) {
+      
+      if(root==nullptr)
+      {
+        return nullptr;
+      }
+      
+      if(root->val<key)   // 删除节点在右子数中
+      {
+        root->right=deleteNode(root->right,key);    ///少了root.left
+        return  root;////// 
+      }
+     else if(root->val>key)   // 删除节点在左子数中
+      {
+       root->left=deleteNode(root->left,key);   
+        return  root;////// 
+      }
+      else      //删除节点为根节点
+      {
+        
+        if(root->left==nullptr){return root->right;}
+       else if(root->right==nullptr){return root->left;}
+       else{
+          TreeNode *nodeMin=min(root->right);
+          nodeMin->right=root->right;         
+          nodeMin->left=root->left;
+          return nodeMin;
+          
+        }        
+      }
+        return nullptr;      
+    
+    }  
+  
+   TreeNode * deleteMin(TreeNode * node)   //调用时要保证左右子数都有
+   {
+     
+     if(node->left==nullptr)
+     {
+       return node->right;
+     }
+     node->left=deleteMin(node->left);
+     return node;
+       
+   }
+  
+  
+  
+    TreeNode* min(TreeNode * node)
+    {
+      while(node->left!=nullptr)
+      {
+        node=node->left;
+      }
+      return node;
+      
+    }
+  
+};
