4b74de884e409d6edc502823a77359d38256f6db
as010101 <37323057+as010101@users.noreply.github.com>
2019-08-29 16:51:05 +0800

    Update 每日温度.cpp

--- 算法/每日温度.cpp
+++ 算法/每日温度.cpp
@@ -1,6 +1,34 @@
 
 
 
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& T) {
+        vector<int>  res(T.size(),0);    
+        for(int i=T.size()-2;i>=0;i--)
+        {
+            for(int j=i+1;j<T.size();j+=res[j])               //注意j+=res[j]
+            {
+                
+            if(T[i]<T[j])
+            {
+                res[i]=j-i;                     //注意j-i                
+                break;
+                
+            }
+                
+            else if(res[j]==0)     //注意是  j
+            {
+                res[i]=0;
+                break;
+            }
+                
+            }       
+        }
+     return res;   
+    }
+};
+
 
 
 class Solution {
