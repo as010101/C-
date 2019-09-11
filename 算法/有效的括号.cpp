ca83beb1e8e4f803c644393ffc7cd571e4f25cf4
as010101 <37323057+as010101@users.noreply.github.com>
2019-09-11 15:03:21 +0800

    Update 有效的括号.cpp

--- 算法/有效的括号.cpp
+++ 算法/有效的括号.cpp
@@ -39,3 +39,27 @@ public:
         else{return true;};
     }
 };
+
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> m_stack;
+        for(const auto& v :s)
+        {
+            if(m_stack.empty())
+                m_stack.push(v);
+            else if(compare(m_stack.top(),v))
+                m_stack.pop();
+            else
+                m_stack.push(v);
+        }
+        return m_stack.size() == 0 ? true : false;
+    }
+private:
+    bool compare(const char& c1,const char& c2)
+    {
+        return (c1 == '[' && c2 == ']') || (c1 == '(' && c2 == ')') || (c1 == '{' && c2 == '}');
+    }
+};
