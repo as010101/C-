64c7424413667736356e9fbac29b4236aa942d88
as010101 <37323057+as010101@users.noreply.github.com>
2019-09-23 18:33:41 +0800

    Update 设计内存文件系统.cpp

--- 算法/设计内存文件系统.cpp
+++ 算法/设计内存文件系统.cpp
@@ -3,9 +3,11 @@ public:
     FileSystem() {
         dirs["/"];
     }
+    // dirs<string,set<string>>  path和path下对应的东西（包括子目录和文件）
+    // files<string,string>      文件path和文件内容
     
     vector<string> ls(string path) {
-        if (files.count(path)) {
+        if (files.count(path)) {                //该路径在为文件
             int idx = path.find_last_of('/');
             return {path.substr(idx + 1)};
         }
@@ -25,13 +27,14 @@ public:
         }
     }
     
-    void addContentToFile(string filePath, string content) {
-        int idx = filePath.find_last_of('/');
+    void addContentToFile(string filePath, string content) {         // 先从给定路径寻找最后次/标识，结果出意外后面会有处理，此处只对正常情况
+        int idx = filePath.find_last_of('/');                        //下处理，如果得到的是正常路径，就直接在该文件名加入内容，该内容存在另一个
+                                                                     //容器中
         string dir = filePath.substr(0, idx);
         string file = filePath.substr(idx + 1);
-        if (dir.empty()) dir = "/";
+        if (dir.empty()) dir = "/";  //无路径时默认将文件存在根路径里
         if (!dirs.count(dir)) mkdir(dir);
-        dirs[dir].insert(file);
+        dirs[dir].insert(file);          //此处的set没有该file会插入，有时插入也不会报错，同时set中依旧只有一份
         files[filePath].append(content);
     }
     
