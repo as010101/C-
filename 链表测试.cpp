cd6364bf269b8bca068aa876c4cd72708e674f93
as010101 <37323057+as010101@users.noreply.github.com>
2019-12-04 19:03:04 +0800

    Update 链表测试.cpp

--- 链表测试.cpp
+++ 链表测试.cpp
@@ -1,3 +1,5 @@
+
+
 typedef struct ListNode
 {
 	int data;
@@ -7,7 +9,7 @@ typedef struct ListNode
 
 void fun(ListNode* node)
 {
-	if (node == nullptr || node->next == nullptr)
+	if (node == nullptr)
 	{
 		return;
 
@@ -17,16 +19,14 @@ void fun(ListNode* node)
 
 }
 
-
-
 int main()
 {
 	ListNode head;
 	head.data = 1000;
 	head.next = nullptr;
 	ListNode* phead= &head;    ///此处要么取地址，要么malloc出地址
+	ListNode* q = &head;
 	phead->next = nullptr;
-	phead->data = 0;
 	for(int i = 0; i < 10; i++)
 	{
 		ListNode* p=(ListNode *)malloc(sizeof(ListNode*));
@@ -34,8 +34,9 @@ int main()
 		p->next = phead->next;  //头插     5 4 3 2  1  0
 		phead->next = p;
 }
-	fun(phead);
-
+	fun(phead);   //  phead是指向1000  phead的next是指向9的  不是phead指向9   phead一直是头结点
+			//也可以不要头结点，每新增一个节点就是头结点
+	cout << endl;
 	while (phead!= nullptr)
 	
 	{
@@ -49,4 +50,4 @@ int main()
 	cin >> c;
 
 }
-// 1234567890  09876543210
+// 0123456789 1000         1000 9876543210
