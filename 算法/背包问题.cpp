87722622e7246dad0ab3400a1564a5fbbd231602
as010101 <37323057+as010101@users.noreply.github.com>
2019-12-05 13:44:59 +0800

    Update 背包问题.cpp

--- 算法/背包问题.cpp
+++ 算法/背包问题.cpp
@@ -8,7 +8,7 @@ dp[0...n][j]                  当在i的条件下循环完j  则已得到d[i][w]
                               d[i+1][w]的值就是原来d[i][w]的值
                               对 max(dp[i-1][j], dp[i-1][j-w[i]] + v[i])的解释  (比较的是  没加i物品的最优--- 腾出位置给i，即在j-w[i]容量的容量下的
                               最优d[i-1][j-w[i] 加上v[i]带来的价值)
-                              j < w[i] 就是意味着当前i无法更新的  d[i][j]只能为原值，因为w[i]甚至都放不进去。。。                              
+                              j < w[i] 就是意味着当前i无法更新的  d[i][j]只能为原值，因为w[i]甚至都放不进去。。。     只能j++然后再看，即容量增加                          
 
 
  ---  dp[i][j]          arr[0-i-1]     arr[i]代表第i种物品的价值
@@ -16,16 +16,16 @@ dp[0...n][j]                  当在i的条件下循环完j  则已得到d[i][w]
 ---- d[i][j]  就是遍历了i种物品，在j容量下所能获得的最大价值
 
 
-int solve(int n, int W) {                //n为数组长度  w为背包容量   w[i] 表示i物品所占背包容量
+int solve(int n, int W,int *w) {                //n为数组长度  w为背包容量   w[i] 表示i物品所占背包容量
     memset(dp, 0, sizeof(dp));
     for (int i = 1; i <= n; i++) {   //i从1开始，因为i=0的值已经确定为0
         for (int j = 0; j <= W; j++) {                 
    
-            if (j < w) {        
+            if (j < w[i]) {        
                 dp[i][j] = dp[i-1][j];
             }
             else {
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v[i]);
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i]] + v[i]);
             }
         }
     }
