7704f4173e5e16f1fc52d83bed55d50bcc2e2419
as010101 <37323057+as010101@users.noreply.github.com>
2019-12-15 18:07:57 +0800

    Update 字符测试.cpp

--- 一些测试/字符测试.cpp
+++ 一些测试/字符测试.cpp
@@ -13,3 +13,27 @@ char b[] = {'a','a', 'a', 'a','a', 'a', 'a',};
 	cin>>c;
 	return 0;
 }
+
+
+
+int main()
+{
+
+	char sz[1024];
+	char *q = sz;
+	string qq="123333";
+
+	string *k = &qq;
+
+	string  s("");
+	while (*q!=char("123"))   //注意此处用临时变量，否则const不能对等
+	{
+		string s("ss");
+		cin.getline(sz, 1024);
+		s = *q;   //s只会存一个字符
+		s = *k;  //s存一串字符
+		cin.clear();
+	}
+
+
+}
