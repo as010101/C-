b9fd10f02721a5a3f27cba58019b1b244be552e7
as010101 <37323057+as010101@users.noreply.github.com>
2019-12-23 15:20:47 +0800

    Update 内存测试.cpp

--- 一些测试/内存测试.cpp
+++ 一些测试/内存测试.cpp
@@ -15,3 +15,55 @@ int main()
 	getchar();
 
 }
+
+
+
+
+
+
+
+
+void* operator new (std::size_t size) throw (std::bad_alloc); //会抛异常
+nothrow (2)	
+void* operator new (std::size_t size, const std::nothrow_t& nothrow_value) throw();   //不会抛异常，返回空指针
+placement (3)	
+void* operator new (std::size_t size, void* ptr) throw();
+
+
+struct MyClass {
+	int data[100];
+	MyClass() { 
+		std::cout << "constructed [" << this << "]\n"; 
+	}
+	MyClass(int a) { std::cout << "constructed [" << this << "]\n"; }
+};
+
+int main() {
+
+	std::cout << "1: ";
+	MyClass* p1 = new MyClass;   //	MyClass* p1 = new MyClass(); 也可以
+	// allocates memory by calling: operator new (sizeof(MyClass))
+	// and then constructs an object at the newly allocated space
+
+	std::cout << "2: ";
+	MyClass* p2 = new (std::nothrow) MyClass;
+	// allocates memory by calling: operator new (sizeof(MyClass),std::nothrow)
+	// and then constructs an object at the newly allocated space
+
+	std::cout << "3: ";
+	new (p2) MyClass(5);
+	// does not allocate memory -- calls: operator new (sizeof(MyClass),p2)
+	// but constructs an object at p2
+
+// Notice though that calling this function directly does not construct an object:
+	std::cout << "4: ";
+	MyClass* p3 = (MyClass*) ::operator new (sizeof(MyClass));
+	// allocates memory by calling: operator new (sizeof(MyClass))
+	// but does not call MyClass's constructor
+
+	delete p1;
+	delete p2;
+	delete p3;
+
+	return 0;
+}
