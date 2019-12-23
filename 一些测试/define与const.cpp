b6986fd9de0899e9a4862bed817c65a653083a2c
as010101 <37323057+as010101@users.noreply.github.com>
2019-12-23 13:21:56 +0800

    Update and rename define.cpp to define与const.cpp

--- 一些测试/define与const.cpp
+++ 一些测试/define与const.cpp
@@ -19,3 +19,20 @@ int main (int argc, char* argv[]) {
 
 name: foo   value: 0
 name: bar   value: 1
+
+    
+    
+    
+    int c = 1;
+
+const int* fun()
+{
+	return &c;
+}
+
+int main()
+{
+	const int *p = (int *)malloc(sizeof(int));    //返回的一定要有const指针限制，不然会报错，但是copy value返回没所谓，原因是返回将值拷贝到了临时空间
+	p=fun();
+
+	}
